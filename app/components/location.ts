d2c2f80d480f28ba954cb1d03892853407ca2e65
Freakyab <aryanbhisikar1@gmail.com>
2024-08-13 23:14:13 +0530

    chore: Update dependencies and add map component

---
+++ app/components/location.ts
@@ -0,0 +1,9 @@
+const markers = [
+    { position: [28.6139, 77.209], label: "New Delhi" },
+    { position: [19.076, 72.8777], label: "Mumbai" },
+    { position: [13.0827, 80.2707], label: "Chennai" },
+    { position: [22.5726, 88.3639], label: "Kolkata" },
+    { position: [12.9716, 77.5946], label: "Bangalore" },
+];
+
+export default markers;
---
+++ app/components/map.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { TileLayer, MapContainer, Marker, Popup, useMap } from "react-leaflet";
+import { userIcon } from "./icons";
+import "leaflet/dist/leaflet.css";
+import markers from "./location";
+import { useEffect } from "react";
+
+function FlyToMarker({ flyTo }: { flyTo: { lat: number; lng: number } }) {
+  const map = useMap();
+  useEffect(() => {
+    if (flyTo.lat !== 0 && flyTo.lng !== 0) {
+      map.flyTo([flyTo.lat, flyTo.lng], 12);
+    }
+  }, [flyTo, map]);
+
+  return null;
+}
+
+export default function Map({
+  cardRefs,
+  flyTo,
+  className,
+  setActiveIndex,
+}: {
+  cardRefs: React.RefObject<HTMLDivElement>[];
+  flyTo: { lat: number; lng: number };
+  className: string;
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
+}) {
+  return (
+    <MapContainer
+      center={[19, 76]}
+      zoom={7}
+      className={className}
+      scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <FlyToMarker flyTo={flyTo} />
+      {markers.map((marker, index) => (
+        <Marker
+          ref={cardRefs[index]}
+          key={index}
+          position={marker.position}
+          eventHandlers={{
+            click: (event: any) => {
+              const map = event.target._map;
+              if (map) {
+                map.flyTo(marker.position, 12);
+                setActiveIndex(index);
+              }
+            },
+          }}
+          icon={userIcon}>
+          <Popup>{marker.label}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
